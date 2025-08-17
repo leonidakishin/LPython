0bf31e78d2137490a3f76f237fc5b19afdd20c59
Leonid Akishin <lakishin@gmail.com>
2025-08-17 07:02:05 +0300

    последнее редактирование 17082025

--- tuple/tmp.py
+++ tuple/tmp.py
@@ -1,3 +1,6 @@
-number_tuple = (1, 2, 3, 4, 5)
-number_list = list(number_tuple)
-print(number_list)
+tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+new_tuples = []
+for e in tuples:
+    e_tmp = e[:-1] + (100,)
+    new_tuples.append(e_tmp)
+print(new_tuples)

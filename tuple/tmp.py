a853dff319e3a3e52d86207dd244b0c8b73c9914
Leonid Akishin <lakishin@gmail.com>
2025-08-19 05:25:58 +0300

    еще

--- tuple/tmp.py
+++ tuple/tmp.py
@@ -1,8 +1,9 @@
-numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+cnt = int(input())
 
-lst_sum = []
+lst = []
 
-for i in range(len(numbers)):
-    lst_sum.append(sum(numbers[i])/len(numbers[i]))
-    
-print(lst_sum)   
+for i in range(cnt):
+    lst.append(input().split())
+
+print(lst)    
+

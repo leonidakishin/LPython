97d749f2d9bd28ff15a5357bb0dbfd9df8bddc2e
Leonid Akishin <lakishin@gmail.com>
2025-09-05 10:16:51 +0300

    05092025_1016

--- random/random_tmp.py
+++ random/random_tmp.py
@@ -1,28 +1,34 @@
-import random as r3
-ucheniki_lst = ['Светлана Зуева', 'Аркадий Белых', 'Борис Боков']
-#n = int(input())
-#ucheniki_lst = [input() for i in range(n)]
-n = 3
-ucheniki_set = set(ucheniki_lst)
+    import random as r3
+    #ucheniki_lst = ['Светлана Зуева', 'Аркадий Белых', 'Борис Боков']
+    n = int(input())
+    ucheniki_lst = [input() for i in range(n)]
+    n = 3
+    ucheniki_set = set(ucheniki_lst)
 
-dct_pary = {}
+    dct_pary = {}
 
-for i in range(n):
-    set_i = set()
-    set_i.add(ucheniki_lst[i])  
-    print(set_i,'set_i')  
-    tmp_set = ucheniki_set - set_i
+    for i in range(n-2):
+        set_i = set()
+        set_i.add(ucheniki_lst[i])  
     
-    str_tmp_set = ''.join(tmp_set)
-    print(str_tmp_set,'tmp')
-   
-    if len(ucheniki_set) == 2 and str_tmp_set == ucheniki_lst[i]:
-        print(ucheniki_set)
-        tmp_set = ucheniki_set - tmp_set
-    dct_pary[ucheniki_lst[i]] = tmp_set.pop()
-    print(dct_pary,'dct')
-    ucheniki_set.remove(dct_pary[ucheniki_lst[i]])
-    tmp_set.clear()
+        tmp_set = ucheniki_set - set_i
+        dct_pary[ucheniki_lst[i]] = tmp_set.pop()
+        ucheniki_set.remove(dct_pary[ucheniki_lst[i]])
+        tmp_set.clear()
 
-print(dct_pary)
+    print(ucheniki_set,'ucheniki_seti') 
+
+    uchenik_tmp1 = ucheniki_set.pop()
+    print(ucheniki_set,'ucheniki_set') 
+    uchenik_tmp2 = ucheniki_set.pop()
+    if ucheniki_lst[-2] !=  uchenik_tmp1 and ucheniki_lst[-1] !=  uchenik_tmp2:
+        dct_pary[ucheniki_lst[-2]] = uchenik_tmp1
+        dct_pary[ucheniki_lst[-1]] = uchenik_tmp2
+    elif ucheniki_lst[-2] ==  uchenik_tmp1 or ucheniki_lst[-1] ==  uchenik_tmp2:
+        dct_pary[ucheniki_lst[-2]] = uchenik_tmp2
+        dct_pary[ucheniki_lst[-1]] = uchenik_tmp1   
+
+
+
+    print(dct_pary)
 

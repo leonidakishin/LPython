38199ab5f76ff557f3faf46a483a73c94f85baab
Leonid Akishin <lakishin@gmail.com>
2025-09-12 04:06:48 +0300

    12092025_0406

--- functions/func_tmp.py
+++ functions/func_tmp.py
@@ -1,16 +1,17 @@
-# matrix = []
-def matrix(n_l=1, m_l=0, value_l=0):
-    if n_l == 1:
-        for _ in range(n_l):
-            matrix_l = [[value_l] * n_l for _ in range(n_l)]
-    if n_l != 1 and m_l == 0:
-        for _ in range(n_l):
-            matrix_l = [[value_l] * n_l for _ in range(n_l)]
-    if n_l != 1 and m_l != 0:
-        for _ in range(n_l):
-            matrix_l = [[value_l] * m_l for _ in range(n_l)]
-    return matrix_l
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
 
+n= int(input())
 
+def f1(kortej):
+    return kortej[0]
+def f2(kortej):
+    return kortej[1]
+def f3(kortej):
+    return kortej[2]
+def f4(kortej):
+    return kortej[3]
 
-print(matrix())
+g = [f1,f2,f3,f4]
+athletes = sorted(athletes,key=g[n-1])
+for e in athletes:
+    print(*e)
